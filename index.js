6581b9c3b87803be6fecf2141cc1b0739fb54489
JiannyAyala <jiannyjj@gmail.com>
2025-01-23 09:38:06 -0500

    declared functions

---
+++ index.js
@@ -0,0 +1,12 @@
+function ConvertToCelcius(Farenheight) {
+    const ConvertToCelcius = Farenheight * 0.556
+    return ConvertToCelcius
+}
+if (ConvertToCelcius <=0) {
+    
+}
+function DescribeTemperature(Description) {
+    const DescribeTemperature
+    return DescribeTemperature 
+}
+
